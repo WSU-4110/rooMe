9035f15e46bfc55e145ae75aa256c9de9ad680e9
Sai Sravani Sure <saisravanisure@gmail.com>
2023-02-07 17:37:28 -0500

    Intial setup firebase.js configuration file

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzbViT2nDpOHEXWVq1SkoJivZ-J-kTo18",
+  authDomain: "roome-45b2c.firebaseapp.com",
+  projectId: "roome-45b2c",
+  storageBucket: "roome-45b2c.appspot.com",
+  messagingSenderId: "32515081126",
+  appId: "1:32515081126:web:bc8c140a291ae613454567"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
